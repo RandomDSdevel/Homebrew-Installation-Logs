c598e770c9e5c095a0d0ef07bf85eb262c7050af
Bryce Glover <RandomDSdevel@gmail.com>
2016-11-09 18:22:46 -0500

    Adds some new logs.
    These are from yet another failed attempt to build Qt v5.7.0 on my setup.

---
+++ Qt/v5.7.0/Third Recorded Attempt/01.configure.cc
@@ -0,0 +1,8 @@
+clang++ called with: -pipe -stdlib=libc++ -xc++ -E -v -
+superenv added:    -isystem/usr/local/include -isystem/usr/include/libxml2 -isystem/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers
+superenv executed: clang++ -pipe -stdlib=libc++ -xc++ -E -v - -isystem/usr/local/include -isystem/usr/include/libxml2 -isystem/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers
+
+clang++ called with: -pipe -stdlib=libc++ -xc++ -E -v -
+superenv added:    -isystem/usr/local/include -isystem/usr/include/libxml2 -isystem/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers
+superenv executed: clang++ -pipe -stdlib=libc++ -xc++ -E -v - -isystem/usr/local/include -isystem/usr/include/libxml2 -isystem/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers
+
